1836f0330467624749c56e1179ab418308e5052e
Ashtonval <tennisjedi20xx@gmail.com>
2023-02-22 09:56:41 -0800

    Completed Many Challenges
    Finished most of the challenge parts.

---
+++ PlayerControl/Assets/Scripts/FollowPlayerFirstPerson.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerFirstPerson : MonoBehaviour
+{
+    public GameObject FirstPerson;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            FirstPerson.SetActive(true);
+        }
+        else()
+    }
+}
